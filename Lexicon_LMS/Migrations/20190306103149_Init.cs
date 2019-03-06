f3edd47b8083b52bad2954175615760ee7bffe74
Göran Olson <goran_6969@outlook.com>
2019-03-06 13:29:07 +0100

    After create user you redirects to Courses

--- Lexicon_LMS/Migrations/20190306103149_Init.cs
+++ Lexicon_LMS/Migrations/20190306103149_Init.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Lexicon_LMS.Migrations
 {
-    public partial class Initial4 : Migration
+    public partial class Init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
