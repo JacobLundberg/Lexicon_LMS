6dc49b8ea981409753f1f611c3043709869ba429
Göran Olson <goran_6969@outlook.com>
2019-03-15 15:53:29 +0100

    StudentStartPage

--- Lexicon_LMS/wwwroot/js/site.js
+++ Lexicon_LMS/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleDocs(id) {
+    // obj.style.color = "purple";
+
+    var docBox = document.getElementById("Docs-" + id);
+    if (docBox.style.display == "none")
+        docBox.style.display = "block";
+    else
+        docBox.style.display = "none";
+
+    // Docs-@item.Id
+
+    return false;
+}
+
+
