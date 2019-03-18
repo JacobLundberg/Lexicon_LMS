159bcd98aa07ec1024802e8ce039518b67f152b8
Jacob Lundberg <jacob_lundberg@hotmail.com>
2019-03-18 13:59:46 +0100

    CRUD Document. Lagt till LMSFormFile & DocumentViewModel

---
+++ Lexicon_LMS/ViewModels/DocumentFileViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lexicon_LMS.ViewModels
+{
+    public class DocumentFileViewModel
+    {
+        [Required]
+        [Display(Name = "Namn")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Beskrivning")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [Required]
+        [Display(Name = "Användare")]
+        public string ApplicationUserId { get; set; }
+
+        [Display(Name = "Kurs")]
+        public int? CourseId { get; set; }
+
+        [Display(Name = "Modul")]
+        public int? ModuleId { get; set; }
+
+        [Display(Name = "Aktivitet")]
+        public int? ActivityId { get; set; }
+
+        public LMSFormFile LMSFormFile { get; set; }
+    }
+}
